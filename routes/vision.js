c5091ae6d7bf3bb1d4c00f31e35424df5f9d6175
Manideep519 <manideepyadav359@gmail.com>
2023-11-02 17:26:00 +0530

    Implemented AWS Rekognition API.

--- routes/vision.js
+++ routes/vision.js
@@ -1,16 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+router.post("/classify", async function (req, res, next) {
+  let imageData = req.files.file;
+  var AWS = require("aws-sdk");
 
-  // Your code starts here //
+  var credentials = new AWS.Credentials(process.env.ACCESS_KEY, process.env.SECRET_KEY);
+  AWS.config.credentials = credentials;
+  AWS.config.update({ region: "ap-southeast-1" });
 
-  // Your code ends here //
+  const client = new AWS.Rekognition();
+  const params = {
+    Image: {
+      Bytes: imageData.data,
+    },
+  };
 
-  res.json({
-    "labels": response
+  client.detectLabels(params, function (err, response) {
+    if (err) {
+      res.status(err.statusCode).json({ error: err.message });
+    } else {
+      let resultLables = [];
+
+      response?.Labels.forEach((label) => {
+        resultLables.push(label?.Name);
+      });
+
+      res.status(200).json({
+        labels: resultLables,
+      });
+    }
   });
 });
 
